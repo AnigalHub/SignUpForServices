8c8b172bb0ec74c814a61a694c188198ad91abde
AnigalHub <anigal1997@gmail.com>
2024-01-13 21:26:01 +0300

    удаление и компонента под логотип

---
+++ src/components/logo.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="logo">
+        <div class="short_name">C&S</div>
+        <div class="long_name">client & specialist</div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'logo'
+  }
+</script>
+
+<style scoped lang="scss">
+.logo{
+    text-align: center;
+    font-family: 'Miltonian Tattoo', serif;
+    .short_name{
+        margin-top: -5px;
+        font-size: 1.25rem;
+    }
+    .long_name{
+        margin-top: -10px;
+    }
+}
+</style>
