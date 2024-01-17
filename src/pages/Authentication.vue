14228591a95d0c89c253c1675b5189fe2da4d309
AnigalHub <anigal1997@gmail.com>
2024-01-17 21:27:59 +0300

    страничка авторизации

---
+++ src/pages/Authentication.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="authentication">
+        <h3>Войдите или Зарегистрируйтесь  </h3>
+        <h5>Добро пожаловать в <span>Client & Specialist:</span></h5>
+        <div class="buttons">
+            <Button :styles="styles.button_styles" text="Войти"/>
+            <Button :styles="styles.button_styles" text="Регистрация"/>
+            <Button :styles="styles.button_back_styles" :stylesIcon="styles.button_back_icon_styles" text="Назад"/>
+        </div>
+    </div>
+</template>
+
+<script>
+  import styles from '../../public/styles.json';
+  import Button from '@/components/Button'
+  export default {
+    name: 'Authentication',
+    components: { Button },
+    data(){
+      return{
+        styles: styles,
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
