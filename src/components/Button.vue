63f3c39e8965343930777d661d22886d8171bc34
AnigalHub <anigal1997@gmail.com>
2024-01-19 23:08:30 +0300

    svg изменено на b-icon

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,14 +1,10 @@
 <template>
     <div>
         <b-button v-if="text === 'Назад'" :style="styles">
-            <span>
-               <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 23 23" :style="stylesIcon">
-                <path  :style="stylesIcon" d="M21.5611 0.980469C21.3045 0.980469 21.1028 1.22224 21.1028 1.52996V10.3218C21.1028 12.4429 19.6635 14.1683 17.8941 14.1683H1.01651C1.08068 13.9375 1.19069 13.7287 1.34654 13.5418L6.30623 7.39853C6.48041 7.17873 6.48041 6.83805 6.29706 6.61825C6.11371 6.40944 5.82951 6.40944 5.64616 6.62924L0.686476 12.7725C-0.202784 13.8386 -0.202784 15.5859 0.686476 16.652L5.64616 22.7953C5.73783 22.9052 5.85701 22.9601 5.97619 22.9601C6.09537 22.9601 6.20538 22.9052 6.29706 22.8063C6.48041 22.5975 6.48041 22.2458 6.30623 22.026L1.33738 15.8827C1.19069 15.7068 1.08068 15.487 1.01651 15.2672H17.8941C20.1677 15.2672 22.0195 13.0473 22.0195 10.3218V1.52996C22.0195 1.22224 21.8178 0.980469 21.5611 0.980469Z"/>
-               </svg>
-            </span>
+            <b-icon icon="arrow-return-left" :style="stylesIcon"></b-icon>
             <span>{{text}}</span>
         </b-button>
-        <b-button v-else :style="styles">{{text}}</b-button>
+        <router-link v-else :to="{name: href}" tag="button" :style="styles">{{text}}</router-link>
     </div>
 </template>
 
@@ -16,6 +12,7 @@
   export default {
     name: 'Button',
     props:{
+      href: String,
       text: String,
       styles: {
         type: Object,
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,23 @@
 <template>
-    <div class="navbar">
+    <b-navbar class="navbar-expand-xl sticky-top " toggleable>
         <b-container>
-            <logo />
-            <div class="menu">
-                <div v-for="(list,index) in information.client_menu" :key="index">{{list}}</div>
-            </div>
-            <svg class="person_icon" xmlns="http://www.w3.org/2000/svg" width="41" height="41" viewBox="0 0 41 41">
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M24.1253 6.07665C21.8578 3.99385 18.1813 3.99385 15.9138 6.07665C13.6462 8.15946 13.6462 11.5363 15.9138 13.6191C18.1813 15.7019 21.8578 15.7019 24.1253 13.6191C26.3928 11.5363 26.3928 8.15946 24.1253 6.07665ZM13.1766 3.56249C16.9558 0.0911489 23.0833 0.0911489 26.8625 3.56249C30.6418 7.03383 30.6418 12.662 26.8625 16.1333C23.0833 19.6046 16.9558 19.6046 13.1766 16.1333C9.39728 12.662 9.39728 7.03383 13.1766 3.56249ZM6.48656 24.3023C10.2191 22.0188 15.12 20.7738 20.0195 20.7738C24.919 20.7738 29.82 22.0188 33.5525 24.3023C37.2983 26.5939 40.0195 30.0498 40.0195 34.398V36.7712C40.0195 38.8304 38.378 40.922 35.826 40.922H4.21308C1.66108 40.922 0.0195312 38.8304 0.0195312 36.7712V34.398C0.0195312 30.0498 2.74091 26.5939 6.48656 24.3023ZM8.63251 27.2615C5.63108 29.0978 3.89051 31.5653 3.89051 34.398V36.7712C3.89051 37.0083 3.98228 37.1826 4.07396 37.2789C4.11858 37.326 4.15681 37.3482 4.17791 37.3575C4.19641 37.3657 4.20596 37.3664 4.21308 37.3664H35.826C35.833 37.3664 35.8428 37.3657 35.861 37.3575C35.8823 37.3482 35.9205 37.326 35.965 37.2789C36.0568 37.1826 36.1485 37.0083 36.1485 36.7712V34.398C36.1485 31.5653 34.408 29.0978 31.4065 27.2615C28.392 25.4172 24.2608 24.3293 20.0195 24.3293C15.7783 24.3293 11.647 25.4172 8.63251 27.2615Z" fill="black"/>
-            </svg>
+            <b-navbar-brand to="/">
+                <logo />
+            </b-navbar-brand>
+            <b-navbar-toggle target="navbar-toggle-collapse">
+                <template v-slot:default="{ expanded }">
+                    <b-icon v-if="expanded" icon="chevron-bar-up"></b-icon>
+                    <b-icon v-else icon="chevron-bar-down"></b-icon>
+                </template>
+            </b-navbar-toggle>
+            <b-collapse id="navbar-toggle-collapse" class="justify-content-center" is-nav>
+                <b-navbar-nav>
+                    <b-nav-item  class="menu" v-for="(list,index) in information.client_menu" to="" :key="index">{{list}}</b-nav-item>
+                </b-navbar-nav>
+            </b-collapse>
+            <b-icon icon="person" width="30" height="30"></b-icon>
         </b-container>
-    </div>
+    </b-navbar>
 </template>
 
 <script>
