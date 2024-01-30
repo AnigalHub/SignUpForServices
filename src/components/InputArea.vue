ff435d13d80c2b01bdfab1ef937d88b8119411a4
AnigalHub <anigal1997@gmail.com>
2024-01-31 01:12:23 +0300

    добавлен цикл инпутов

---
+++ src/components/InputArea.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="inputArea">
+        <div v-for="(value,index) in loginUp">
+            <b-form-group :label="value.label" :label-for="index" label-cols-sm="1">
+                <b-form-input :id="index"></b-form-input>
+            </b-form-group>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'InputArea',
+    props:{
+      loginUp: Array,
+    },
+  }
+</script>
+
+<style scoped>
+    .inputArea{
+        background: white;
+    }
+    .form-control {
+        border: white;
+    }
+</style>
