ddcb65aa58562096adc31a91d6fec0e69c326786
AnigalHub <anigal1997@gmail.com>
2024-02-02 15:50:04 +0300

    добавлена компонента inputArea и данные для инпутов

--- src/pages/LoginUp.vue
+++ src/pages/LoginUp.vue
@@ -2,7 +2,7 @@
     <div class="loginUp">
         <h3>Вход</h3>
         <h5>Логин и пароль</h5>
-
+        <InputArea :loginUp="information.login_up"/>
         <div class="buttons">
             <Button :styles="styles.button_styles" text="Войти"/>
             <Button :styles="styles.button_back_styles" :stylesIcon="styles.button_back_icon_styles" text="Назад"/>
@@ -13,13 +13,16 @@
 <script>
   import Button from '@/components/Button'
   import styles from '../../public/styles.json'
+  import InputArea from '@/components/InputArea'
+  import information from '../../public/information.json'
 
   export default {
     name: 'LoginUp',
-    components: { Button },
+    components: { InputArea, Button },
     data(){
       return{
         styles: styles,
+        information: information,
       }
     }
   }
