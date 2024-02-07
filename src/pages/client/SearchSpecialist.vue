bff28ba0528d74253437b0d7941ea790613f561b
AnigalHub <anigal1997@gmail.com>
2024-02-08 02:23:04 +0300

    json с будущими данными с базы

--- src/pages/client/SearchSpecialist.vue
+++ src/pages/client/SearchSpecialist.vue
@@ -2,7 +2,7 @@
     <div class="searchSpecialist">
         <h3>Поиск специалиста</h3>
         <FormArea name="search" :data="information.search"/>
-        <PersonCard/>
+        <PersonCard :data="informationDatabase.specialists"/>
         <div class="buttons">
             <Button :styles="styles.button_back_styles" :stylesIcon="styles.button_back_icon_styles" text="Назад"/>
         </div>
@@ -14,6 +14,7 @@
   import FormArea from '@/components/FormArea'
   import styles from '../../../public/styles.json'
   import information from '../../../public/information.json'
+  import informationDatabase from '../../../public/informationDatabase.json'
   import PersonCard from '@/components/PersonCard'
 
   export default {
@@ -23,6 +24,7 @@
       return{
         styles: styles,
         information: information,
+        informationDatabase:informationDatabase
       }
     }
   }
