669cb0c46cf55ee5bcedb235ca02910cf5b5ca36
AnigalHub <anigal1997@gmail.com>
2024-02-19 00:21:43 +0300

    Карты лояльности + Лояльность (вычисление максимального количества услуг до скидки, вывод скидки и звездочек(услуг)

---
+++ src/components/Loyalty.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="loyalty">
+        <div >
+            <b-row v-for="(value) in data" class="discount">
+                <b-col v-for="(number) in maxNumberServices" >
+                    <i v-for="(current_number) in value.number_services" v-if="current_number===number" class="bi bi-star" ></i>
+                </b-col>
+                <b-col class="size_discount">
+                    {{value.discount}}
+                </b-col>
+            </b-row>
+        </div>
+
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Loyalty',
+    props:{
+      name: String,
+      data: Array,
+    },
+    computed:{
+      maxNumberServices(){
+        return this.data.reduce((acc, curr) => acc.number_services > curr.number_services ? acc : curr).number_services
+      }
+    },
+    created () {
+      console.log('data', this.data)
+    }
+  }
+</script>
+
+<style scoped>
+    .discount{
+        margin-bottom: 8px;
+    }
+    .size_discount{
+        font-size: 1.6rem;
+        margin-top: -5px;
+        margin-bottom: 0;
+    }
+    .loyalty{
+        background: white;
+        border-radius: 10px;
+        padding: 20px;
+        width: calc(36px + 100%);
+        margin-left: -18px;
+        box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+        margin-bottom: 1.5rem;
+    }
+</style>
---
+++ src/pages/client/MyCards.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="myCards">
+        <h3>Карты лояльности</h3>
+        <PersonCard name="myCards" :data="informationDatabase.my_specialists"/>
+        <div class="buttons">
+            <Button :styles="styles.button_back_styles" :stylesIcon="styles.button_back_icon_styles" text="Назад"/>
+        </div>
+    </div>
+</template>
+
+<script>
+  import Button from '@/components/Button'
+  import FormArea from '@/components/FormArea'
+  import styles from '../../../public/styles.json'
+  import informationDatabase from '../../../public/informationDatabase.json'
+  import PersonCard from '@/components/PersonCard'
+
+  export default {
+    name: 'MyCards',
+    components: { PersonCard, FormArea, Button },
+    data(){
+      return{
+        styles: styles,
+        informationDatabase:informationDatabase
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
