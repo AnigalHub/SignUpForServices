a34aa0040b7f05d01bd4f23778c36b9974177ed7
AnigalHub <anigal1997@gmail.com>
2024-03-03 01:22:35 +0300

    svg заменена на i

--- src/components/FormArea.vue
+++ src/components/FormArea.vue
@@ -14,10 +14,8 @@
                 <div >
                     <label v-if="value.label" :for="value.label" :type="value.type"> {{value.label}}:</label>
                     <div class="input">
-                        <div class="icon" v-if="name === 'search'">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-                            </svg>
+                        <div class="icon" v-if="name === 'SearchSpecialist'">
+                            <i class="bi bi-search"></i>
                         </div>
                         <b-form-input autocomplete="off"
                                       :placeholder="value.placeholder"
@@ -80,8 +78,11 @@
 
 <style scoped>
     .icon{
-        margin-top: .8%;
-        border-bottom: 2px solid rgba(0, 0, 0, 0.25);
+        margin-top: 1.25%;
+        margin-right: .5%;
+    }
+    .icon i:before {
+        font-size: 1rem;
     }
     .input{
         display: flex;
