d02c774d4eb6cdcf3d3e65cb6f6b44bb7330fa4b
AnigalHub <anigal1997@gmail.com>
2024-05-17 01:16:01 +0300

    добавление загрузки тем, текущей темы и системной темы в стор Настроек Интерфейса

--- src/store/common/SettingsInterfaceStore.js
+++ src/store/common/SettingsInterfaceStore.js
@@ -4,11 +4,32 @@ export default {
   namespaced: true,
   state: {
     title: 'Настройки',
-    items: [],
+    items: informationDatabase.themes,
+    currentTheme: 'light',
+    systemTheme: 'light',
   },
   actions: {
     getData(ctx) {
       ctx.state.items = informationDatabase.themes;
     },
+    loadTheme(ctx){
+      let setSystemTheme = () => {
+        let systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+        ctx.state.systemTheme = systemTheme;
+       ctx.dispatch('setTheme', systemTheme)
+      }
+      window.matchMedia('(prefers-color-scheme: dark)').addListener(function () {
+        setSystemTheme()
+      });
+      setSystemTheme()
+    },
+  },
+  mutations: {
+    selectTheme(state, payload){
+      state.currentTheme = payload;
+    },
+  },
+  getters: {
+    CurrentTheme: (state) => state.currentTheme,
   },
 };
